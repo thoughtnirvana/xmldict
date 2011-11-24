e2ffe8b02e1fca504998d62148f2c74cad17b236
Rahul Kumar <rahulsinner@gmail.com>
2011-11-25 03:35:39 +0530

    Adding fabfile and basic tasks.

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric.api import *
+
+def upload():
+    """
+    Uploads the package to pypi.
+    """
+    local('python setup.py sdist upload')
+    clean()
+
+def _clean():
+    """
+    Cleans generated files while uploading to pypi.
+    """
+    local('rm -rf dist xmldict.egg*')
+
+def test():
+    """
+    Runs unit tests.
+    """
+    local('nosetests -v')
