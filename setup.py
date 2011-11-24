72ce50a24775a06a3d328cd39ce113219c6c69cf
Rahul Kumar <rahulsinner@gmail.com>
2011-11-24 16:36:13 +0530

    First commit.

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+xmldict
+-------
+
+Convert xml to python dictionaries.
+
+"""
+
+from setuptools import setup
+setup(name='xmldict',
+      version='0.1',
+      description='Convert xml to python dictionaries.',
+      long_description=__doc__,
+      author='Rahul Kumar',
+      author_email='rahul@thoughtnirvana.com',
+      license='BSD',
+      url='https://github.com/thoughtnirvana/xmldict',
+      py_modules=['xmldict']
+     )
